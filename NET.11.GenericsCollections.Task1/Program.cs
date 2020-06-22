0b7025f0f75b68311f1dc205569caa3fda7f7be2
Игорь Соколов <strelok111@gmail.com>
2020-06-22 20:24:12 +0300

    Add project files.

---
+++ NET.11.GenericsCollections.Task1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NET._11.GenericsCollections.Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var binaryTree = new BinaryTree<int>();
+
+            binaryTree.Add(8);
+            binaryTree.Add(3);
+            binaryTree.Add(10);
+            binaryTree.Add(1);
+            binaryTree.Add(6);
+            binaryTree.Add(4);
+            binaryTree.Add(7);
+            binaryTree.Add(14);
+            binaryTree.Add(16);
+
+            binaryTree.PrintTree();
+
+            Console.WriteLine(new string('-', 40));
+            binaryTree.Remove(3);
+            binaryTree.PrintTree();
+
+            Console.WriteLine(new string('-', 40));
+            binaryTree.Remove(8);
+            binaryTree.PrintTree();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ NET.11.GenericsCollections.Task1/StudentOnTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace NET._11.GenericsCollections.Task1
+{
+    /// <summary>
+    /// Студент проходящий тест
+    /// </summary>
+    public class StudentOnTest : IComparable<StudentOnTest>
+    {
+        /// <summary>
+        /// Имя студента
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// Название теста
+        /// </summary>
+        public string TestName { get; set; }
+        /// <summary>
+        /// Дата теста
+        /// </summary>
+        public DateTime TestDate { get; set; }
+        /// <summary>
+        /// Оценка затест
+        /// </summary>
+        public byte Assessment { get; set; }
+        /// <summary>
+        /// Сравнение оценки с другим студентом
+        /// </summary>
+        /// <param name="other"></param>
+        /// <returns></returns>
+        public int CompareTo(StudentOnTest other)
+        {
+            if (other == null) return 1;
+            // Сравниваем оценки
+            return Assessment.CompareTo(other.Assessment);
+        }
+    }
+}
